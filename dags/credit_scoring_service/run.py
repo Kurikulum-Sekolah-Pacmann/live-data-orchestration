51c9857bfabc17824b3bb542738578b7b890bfa9
rahilaode <laoderahilatansa@gmail.com>
2024-10-29 04:52:51 +0700

    fix

--- dags/credit_scoring_service/run.py
+++ dags/credit_scoring_service/run.py
@@ -5,7 +5,8 @@ from airflow.decorators import dag
 @dag(
     dag_id = "credit-scoring-service",
     schedule = "@daily",
-    start_date = datetime.datetime(2024, 10, 24)
+    start_date = datetime.datetime(2024, 10, 24),
+    catchup = False
 )
 def credit_scoring_service():
     extraction.extract_credit_data() >> preprocessing.preprocess_credit_data() >> training.training_credit_data()
