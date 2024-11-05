73beac9a3ce40de48c797e44090fbb501d869a1b
rahilaode <laoderahilatansa@gmail.com>
2024-11-05 07:29:26 +0700

    fix

---
+++ dags/dag2.py
@@ -0,0 +1,42 @@
+from airflow.decorators import dag, task, task_group
+from datetime import datetime
+import time
+# Define the DAG using the dag decorator
+@dag(
+    description='A simple DAG',
+    schedule_interval='*/5 * * * *',
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=['example']
+)
+def example_dag2():
+    
+    @task
+    def task_1():
+        time.sleep(10)
+        print("This is task 1")
+        raise Exception("Error")
+    
+    @task
+    def task_2():
+        time.sleep(20)
+        print("This is task 2")
+
+    @task_group
+    def task_groups():
+        @task
+        def task_3():
+            time.sleep(30)
+            print("This is task 3")
+
+        @task
+        def task_4():
+            time.sleep(40)
+            print("This is task 4")
+
+        task_3() >> task_4()
+
+    
+    task_1() >> task_2() >> task_groups()
+
+example_dag2()
